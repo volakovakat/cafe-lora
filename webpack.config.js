5f21ba73e37365ae738a16b7b998c7f0126113a8
Katerina Volakova <volakova.kat@gmail.com>
2022-11-29 19:01:26 +0100

    clean project

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'eval-source-map',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'img/[name]-[hash:6].[ext]'
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        type: 'asset/resource'
+      }
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: '', noErrorOnMissing: true },
+      ],
+    }),
+  ],
+};
