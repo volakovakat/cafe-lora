f279cd7dd03f6c26d81eea038a2e839314a4513e
Katerina Volakova <volakova.kat@gmail.com>
2022-12-06 20:20:05 +0100

    Fetching drinks from the API + Email Authorization

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,84 +1,32 @@
 import "./Menu.css";
 import {Drink} from "./components/Drink/Drink.js";
 
-const drinkProps = {
-   id: 'romano',
-   name: 'Romano',
-   ordered: false,
-   image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
-   layers: [
-      {
-         color: '#fbdf5b',
-         label: 'citrón',
-      },
-      {
-         color: '#613916',
-         label: 'espresso',
-      },
-   ]
-};
+const headers = {"Authorization": "Email volakova.kat@gmail.com"}
 
 export const Menu = () => {
    const element = document.createElement('section');
    element.id = "menu";
    element.classList.add("menu");
-   element.innerHTML = `
-    <div class="container">
-       <h2>Naše nabídka</h2>
-       <p class="menu-intro">
-         Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
-       </p>
-       <div class="drinks-list">
-            ${Drink(drinkProps)}
-         <div class="drink">
-           <div class="drink__product">
-             <div class="drink__cup">
-               <img src="https://apps.kodim.cz/daweb/cafelora/assets/cups/doppio.png">
-             </div>
-             <div class="drink__info">
-               <h3>Doppio</h3>
-               <div class="layer">
-                 <div class="layer__color" style="background-color: #613916"></div>
-                 <div class="layer__label">espresso</div>
-               </div>
-             </div>
-           </div>
-           <div class="drink__controls">
-             <button class="order-btn">
-               Objednat
-             </button>
-           </div>
-         </div>
 
-         <div class="drink">
-           <div class="drink__product">
-             <div class="drink__cup">
-               <img src="https://apps.kodim.cz/daweb/cafelora/assets/cups/lungo.png">
-             </div>
-             <div class="drink__info">
-               <h3>Lungo</h3>
-               <div class="layer">
-                 <div class="layer__color" style="background-color: #b0dee1"></div>
-                 <div class="layer__label">voda</div>
-               </div>
-               <div class="layer">
-                 <div class="layer__color" style="background-color: #613916"></div>
-                 <div class="layer__label">espresso</div>
-               </div>
-             </div>
-           </div>
-           <div class="drink__controls">
-             <button class="order-btn">
-               Objednat
-             </button>
-           </div>
-         </div>
-       </div>
-     
-       <div class="order-detail">
-         <a href="/objednavka">Detail objednávky</a>
-       </div>
-     </div>
-   `;
+   fetch('https://apps.kodim.cz/daweb/cafelora/api/me/drinks', {headers:headers})
+       .then((response) => {
+          return response.json();
+       }).then((data) => {
+          const e = document.querySelector("section.menu");
+          e.innerHTML = `
+          <div class="container">
+            <h2>Naše nabídka</h2>
+            <p class="menu-intro">
+            Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
+            </p>
+            <div class="drinks-list">
+               ${data.results.map(d => Drink(d))}
+            </div>
+            <div class="order-detail">
+                <a href="/objednavka">Detail objednávky</a>
+            </div>
+          </div>
+          `;
+   })
    return element;
 };
--- src/components/Menu/components/Drink/Drink.js
+++ src/components/Menu/components/Drink/Drink.js
@@ -1,31 +1,24 @@
 import "./Drink.css";
 import {Layer} from "../Layer/Layer.js";
 
-const layerProps = {
-    color: '#feeeca',
-    label: 'mléčná pěna',
-}
-
 export const Drink = (props) => {
-    const {name, image} = props;
+    const {layers, name, image} = props;
 
     const element = document.createElement("div");
     element.classList.add("drink");
     element.innerHTML = `
-       <div class="drink__product">
-         <div class="drink__cup">
-           <img src="${image}">
-         </div>
-         <div class="drink__info">
-           <h3>${name}</h3>
-             ${Layer(layerProps)}
-         </div>
-       </div>
-       <div class="drink__controls">
-         <button class="order-btn">
-           Objednat
-         </button>
-       </div>
+        <div class="drink__product">
+            <div class="drink__cup">
+                <img src="${image}">
+            </div>
+            <div class="drink__info">
+                <h3>${name}</h3>
+                ${layers.map(l => Layer(l))}
+            </div>
+        </div>
+        <div class="drink__controls">
+            <button class="order-btn">Objednat</button>
+        </div>
     `;
 
     return element.outerHTML;
